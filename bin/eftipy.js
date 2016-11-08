6ce58946441cfcb25b593c935912ab35220f58ca
coderaiser <coderaiser@cloudcmd.io>
2016-11-08 18:07:01 +0200

    chore(eftipy) rm iife

--- bin/eftipy.js
+++ bin/eftipy.js
@@ -1,41 +1,39 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var eftipy      = require('../'),
+    pipe        = require('pipe-io'),
     
-    var eftipy      = require('../'),
-        pipe        = require('pipe-io'),
-        
-        argv        = process.argv.slice(2),
-        addr        = argv[0],
-        filename    = argv[1],
-        callback    = function(error) {
-            if (error)
-                console.error(error.message);
-        },
-        ftp;
+    argv        = process.argv.slice(2),
+    addr        = argv[0],
+    filename    = argv[1],
+    callback    = function(error) {
+        if (error)
+            console.error(error.message);
+    },
+    ftp;
+
+if (!addr) {
+    console.log('eftipy <host> <filename>');
+} else if (/-v|--version/.test(addr)){
+    console.log(version());
+} else {
+    ftp = eftipy(addr, filename);
     
-    if (!addr) {
-        console.log('eftipy <host> <filename>');
-    } else if (/-v|--version/.test(addr)){
-        console.log(version());
-    } else {
-        ftp = eftipy(addr, filename);
-        
-        ftp.on('error', callback);
-        
-        ftp.on('dir', function(list) {
-            list.forEach(function(file) {
-                console.log(file.name);
-            });
+    ftp.on('error', callback);
+    
+    ftp.on('dir', function(list) {
+        list.forEach(function(file) {
+            console.log(file.name);
         });
-        
-        ftp.on('file', function(stream) {
-            pipe([stream, process.stdout], callback);
-        });
-    }
+    });
     
-    function version() {
-        return 'v' + require('../package.json').version;
-    }
-})();
+    ftp.on('file', function(stream) {
+        pipe([stream, process.stdout], callback);
+    });
+}
+
+function version() {
+    return 'v' + require('../package.json').version;
+}
